b592050dab6fd971c5e19298a32ad928474cc0b0
Eric Lovell <eric1992@vt.edu>
2014-10-28 15:27:42 -0400

    Added nb project.

---
+++ MotifFinder/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Eric
+ *
+ * Created on October 28, 2014, 3:17 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
