e62bc4feae08725f9370ae7859f7fc55bffeb587
Eric Lovell <eric1992@vt.edu>
2014-11-03 18:40:39 -0500

    Refactor

---
+++ MotifFinder/Nucleotide.h
@@ -0,0 +1,13 @@
+/* 
+ * File:   Neocleotide.h
+ * Author: Eric
+ *
+ * Created on November 3, 2014, 1:48 PM
+ */
+
+#ifndef NUCLEOTIDE_H
+#define	NUCLEOTIDE_H
+    enum Nucleotide_t {A, T, G, C, DC};
+
+#endif	/* NEOCLEOTIDE_H */
+
