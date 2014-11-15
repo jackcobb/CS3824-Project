08717bd3ef9c64cbe56c748c33e552dc78a76275
Eric Lovell <eric1992@vt.edu>
2014-11-15 14:22:39 -0500

    Implemented randomPosition for RandomizedSearchEngine.cpp;

--- MotifFinder/RandomizedSearchEngine.cpp
+++ MotifFinder/RandomizedSearchEngine.cpp
@@ -4,8 +4,16 @@
  * 
  * Created on November 15, 2014, 1:43 PM
  */
-
+#include <stdlib.h>
+#include <tgmath.h>
 #include "RandomizedSearchEngine.h"
+
+int RandomizedSearchEngine::randomPosition(int sequence){
+    int maxPos = dna->Size(sequence) - 1;
+    int i = rand();
+    double random = (i / (double)(RAND_MAX)) * maxPos;
+    return (int)floor(random);
+}
 
 RandomizedSearchEngine::~RandomizedSearchEngine() {
 }
